3234448b871b6655dfe6435cedc095e0d2137400
Your Name <you@example.com>
2023-05-09 23:45:43 +0300

    Solved CI

--- include/structs/mt_randomengine.h
+++ include/structs/mt_randomengine.h
@@ -4,7 +4,6 @@
 
 #define P_PI 3.14159265358979323846
 
-int Mersenne_Twister_Generator(const int minvalue, const int maxvalue);
 std::pair<double, double> randomPositionInCircle(const double Cx, const double Cy, const double R);
 std::pair<double, double> randomPositionInRect(const double Px, const double Py, const double Qx, const double Qy);
 
--- source/structs/mt_randomengine.cpp
+++ source/structs/mt_randomengine.cpp
@@ -1,12 +1,5 @@
 #include "structs/mt_randomengine.h"
 
-int Mersenne_Twister_Generator(const int minvalue, const int maxvalue) {
-      std::random_device device;
-      std::mt19937 generator(device());
-      std::uniform_int_distribution<int> distribution(minvalue, maxvalue);
-      return distribution(generator);
-}
-
 // Random position delimited by the circle with center Cx Cy and radius R
 std::pair<double, double> randomPositionInCircle(const double Cx, const double Cy, const double R) {
     std::random_device rd;
