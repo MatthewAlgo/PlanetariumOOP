4312e5bac786b75d79f711d754e3897b0a19ce41
Your Name <you@example.com>
2023-05-10 00:11:23 +0300

    Solved CI

--- include/structs/mt_randomengine.h
+++ include/structs/mt_randomengine.h
@@ -8,4 +8,6 @@ class RandomEngine {
     public:
     static std::pair<double, double> randomPositionInCircle(const double Cx, const double Cy, const double R);
     static std::pair<double, double> randomPositionInRect(const double Px, const double Py, const double Qx, const double Qy);
+    RandomEngine() = delete;
+    ~RandomEngine() = delete;
 };
