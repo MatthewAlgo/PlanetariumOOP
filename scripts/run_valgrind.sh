967dfc5b89ad80919562217d65969d5bd6b741fd
Your Name <you@example.com>
2023-05-09 22:27:39 +0300

    Small fixes

--- scripts/run_valgrind.sh
+++ scripts/run_valgrind.sh
@@ -4,3 +4,4 @@ sleep 1 && xdotool search --name  "${APP_WINDOW}" key Y &&
 sleep 1 && xdotool search --name  "${APP_WINDOW}" key X &&
 sleep 1 && xdotool search --name  "${APP_WINDOW}" key Z &&
 sleep 2 && xdotool search --name  "${APP_WINDOW}" key Alt+F4
+
